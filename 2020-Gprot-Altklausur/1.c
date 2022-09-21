ea77637cfe8e5bd2d8274080a33512e4c3f170c9
Morgall <113110319+Morgall@users.noreply.github.com>
2022-09-21 11:46:45 +0200

    Update 1.c

---
+++ 2020-Gprot-Altklausur/1.c
@@ -0,0 +1,2 @@
+/* Es gibt jeweils nur warnings, ich denke Anweisung 2 ist die mit dem Fehler, weil ein damit man einen Pointer
+auf u bekommen könnte, müsste man *uptr die Adresse von u geben, also *uptr = &u*/
