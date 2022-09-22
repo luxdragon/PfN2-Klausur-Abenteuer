fe08657327abead6bb74fca1acbe920ca1f39fe2
Morgall <113110319+Morgall@users.noreply.github.com>
2022-09-22 15:38:19 +0200

    Create 01.c

---
+++ 2022-Erstklausur/N_zeugs/01.c
@@ -0,0 +1,14 @@
+/*
+egal was c ist, es würde immer geprinted werden
+
+Das ; nach dem if muss weg
+*/
+
+#include<stdio.h>
+
+int main(void){
+  int c = 10;
+
+  if(c < 7)
+    printf("c ist kleiner als 7\n");
+}
